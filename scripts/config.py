2c54e08e6ffde9e6f3cd23d38518bbe9e680afee
Pranav <pranavbalu1@outlook.com>
2025-09-26 17:13:06 -0500

    alter bounding box

--- scripts/config.py
+++ scripts/config.py
@@ -1,20 +1,21 @@
 # Bounding Box for area of interest
-BBOX = [-120.5, 35.0, -118.0, 37.0] # Example: California region
-WIDTH = 1200  # Image width
-HEIGHT = 1200  # Image height
+BBOX = [-118.56, 33.56, -117.5, 34.245] # Example: California region
+WIDTH = 2000  # Image width
+HEIGHT = 1560  # Image height
 
 # AWS S3
 S3_BUCKET = "ndvi-daily-data"
 
 #PREFIX = "sentinel-data" # 1 year of data
 #PREFIX = "test" # 1 month of data
-PREFIX = "test_2" # 1 day of data
+#PREFIX = "test_2" # 1 day of data
+PREFIX = "known_sources" # Known methane sources
 
 
 #Model parameters
 BATCH_SIZE = 100000
 
-CONTAMINATION = 0.01
+CONTAMINATION = 0.005
 RANDOM_STATE = 42
 EPS = 0.01  # DBSCAN eps parameter
 MIN_SAMPLES = 5  # DBSCAN min_samples parameter
