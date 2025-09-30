9e72b5146039f20185968b87be814f2b1cbc166c
Pranav <pranavbalu1@outlook.com>
2025-09-30 04:47:57 -0500

    final commit

--- scripts/config.py
+++ scripts/config.py
@@ -1,18 +1,18 @@
 # Bounding Box for area of interest
-BBOX = [-118.56, 33.56, -117.5, 34.245] # Example: California region
-WIDTH = 2000  # Image width
-HEIGHT = 1560  # Image height
+BBOX = [-117.8, 33.9, -117.4, 34.3] # Example: California region
+WIDTH = 2500  # Image width
+HEIGHT = 2500  # Image height
 
 # AWS S3
 S3_BUCKET = "ndvi-daily-data"
 
-PREFIX = "sentinel-data"
+PREFIX = "sentinel-data-zoomed"
 
 
 #Model parameters
 BATCH_SIZE = 100000
 
-CONTAMINATION = 0.005 # Proportion of outliers in the data
+CONTAMINATION = 0.003 # Proportion of outliers in the data
 RANDOM_STATE = 42
 EPS = 0.01  
-MIN_SAMPLES = 2  # DBSCAN min_samples parameter
+MIN_SAMPLES = 10  # DBSCAN min_samples parameter
